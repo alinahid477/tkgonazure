647fce4e8d2a41395a0abeb636f1bc40aad4d101
Ali Nahid <alinahid@gmail.com>
2021-10-19 18:06:55 +1100

    start for tkgonazure

---
+++ start.sh
@@ -0,0 +1,17 @@
+name=$1
+forcebuild=$2
+if [[ $name == "forcebuild" ]]
+then
+    name=''
+    forcebuild='forcebuild'
+fi
+if [[ -z $name ]]
+then
+    name='tkgonazure'
+fi
+isexists=$(docker images | grep "\<$name\>")
+if [[ -z $isexists || $forcebuild == "forcebuild" ]]
+then
+    docker build . -t $name
+fi
+docker run -it --rm -v ${PWD}:/root/ --add-host kubernetes:127.0.0.1 --name $name $name /bin/bash
